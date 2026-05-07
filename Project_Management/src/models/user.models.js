5a8be1bde2c512bc6f67b0f024d8ff927e6aff9c
itzskgithub <shubhampandey8969@gmail.com>
2026-05-07 10:54:25 +0530

    created jwt for the user both access token and refresh token

--- Project_Management/src/models/user.models.js
+++ Project_Management/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from 'mongoose';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema(
     {
@@ -60,4 +62,38 @@ const userSchema = new Schema(
     },
 );
 
-export const User = mongoose.model("User", userSchema)
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+            email : this.email,
+            username : this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+    },
+    process.env.REFRESH_TOKEEN_SECRET,
+    {expiresIn : process.env.REFRESH_TOKEN_TOKEN}
+)
+}
+
+export const User = mongoose.model("User", userSchema);
