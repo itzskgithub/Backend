8085116cf679e015f3f725dcf45049ce54224cd4
itzskgithub <shubhampandey8969@gmail.com>
2026-05-08 12:43:33 +0530

    Code the validators and middleware for the valid user

--- Project_Management/src/Routes/auth.routes.js
+++ Project_Management/src/Routes/auth.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controllers.js";
+import {validator} from "../middlewares/validator.middlewares.js"
+import { userRegisterValidator } from "../validators/index.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validator, registerUser);
+
+
 
 export default router;
---
+++ Project_Management/src/validators/index.js
@@ -0,0 +1,34 @@
+import {body} from "express-validator"
+
+const userRegisterValidator = () => {
+    return [
+        body("email")
+            //It trims all the data removes extra space from the beginning and end of the data
+            .trim()
+            .notEmpty()
+            //If there is a error in the just above method then withmessage is called here the just above message is notEmpty() so if it fails then withMessage() will be called by the express-validator//
+            .withMessage("Email is required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is required")
+            .isLowercase()
+            .withMessage("username must be in the lowercase")
+            .isLength({min : 3})
+            .withMessage("Username must have atleast three characters"),
+        
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is required"),
+        body("fullName")
+            .optional()
+            .trim(),
+    ]
+}
+
+export {
+    userRegisterValidator
+}
