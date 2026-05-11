1f562c28e1672c590b159bd5e36ac2c76ef5fa41
itzskgithub <shubhampandey8969@gmail.com>
2026-05-11 12:41:01 +0530

    Added routes for all of that

--- Project_Management/src/Routes/auth.routes.js
+++ Project_Management/src/Routes/auth.routes.js
@@ -1,17 +1,52 @@
 import { Router } from "express";
-import { registerUser, login, logoutUser } from "../controllers/auth.controllers.js";
+import { registerUser, 
+  login, 
+  logoutUser, 
+  getCurrentUser,
+  verifyEmail,
+  resendEmailVerification,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  resetForgotPassword,
+  changePassword } from "../controllers/auth.controllers.js";
 import {validate} from "../middlewares/validator.middlewares.js"
 import { userRegisterValidator , userLoginValidator} from "../validators/index.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
+import {
+  userChangeCurrentPasswordValidator,
+  userForgotPasswordValidator,
+  userLoginValidator,
+  userRegisterValidator,
+  userResetForgotPasswordValidator,
+} from "../validators/index.js";
+
 const router = Router();
+
+//unsecured route
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 
 router.route("/login").post(userLoginValidator(), validate, login);
 
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/forgot-password").post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+
+router.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
+
+
+
 //secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+router.route("/change-password").post(verifyJWT, userChangeCurrentPasswordValidator(), validate, changePassword);
+
+router.route("/resend-email-verification").post(verifyJWT, resendEmailVerification);
 
 
 
